8d6a63fe566ff3cc023e97fe126d1631d9fe6b2e
alexkvashin21 <alexkvashin3021@gmail.com>
2024-12-27 01:36:36 +0500

    some temp changes

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTweetRequest;
+use App\Models\Category;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TweetController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $categories = Category::all();
+        $tweets = Tweet::all();
+
+        return view('index', ['categories' => $categories, 'tweets' => $tweets]);
+    }
+
+    public function store(CreateTweetRequest $request)
+    {
+        dd($request->validated());
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TweetController::class, 'index'])->name('tweet.index');
+Route::post('/store', [TweetController::class, 'store'])->name('tweet.store');
